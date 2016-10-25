36e818e970fb61f576b8080ee73bf86b92304ee5
Leonhard Massloch <leuchtmetall@users.noreply.github.com>
2016-10-25 11:02:39 +0900

    main page and flash message design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     if user_signed_in?
       redirect_to month_path(year: Date.today.year, month: Date.today.month)
